24540329dd74b94e94bd9fbc1aeab9d161733ce6
Samra-coding <naveenasenthil597@gmail.com>
2024-11-07 19:31:42 +0530

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  content: ['./src/**/*.{html,js,jsx,ts,tsx}'],
+  theme: {
+    extend: {
+      screens: {
+        'xs':'320px',
+        'xss': '370px', 
+        'xsss': '425px',
+        'lgl':'1200px',
+        'xxl': '1440px',
+        '3xl': '1920px',
+        '4xl': '2560px',
+      },
+    },
+  },
+  plugins: [],
+}
